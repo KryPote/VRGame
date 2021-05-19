a426eb250438ccfbaeb70f3bb0805f5b704a5ff4
KryPote <krystian.potepa@gmail.com>
2021-05-20 00:10:13 +0200

    First of all

---
+++ Assets/Break.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break : MonoBehaviour
+{
+    public GameObject fractured;
+    public float breakForce;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("f"))
+            BreakTheThing();
+    }
+    public void BreakTheThing()
+    {
+        GameObject frac = Instantiate(fractured, transform.position, transform.rotation);
+        foreach(Rigidbody rb in frac.GetComponentsInChildren<Rigidbody>())
+        {
+            Vector3 force = (rb.transform.position - transform.position).normalized * breakForce;
+            rb.AddForce(force);
+        }
+        Destroy(gameObject);
+
+    }
+}
---
+++ Assets/Scripts/SimpleAttach.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SimpleAttach : MonoBehaviour
+{
+
+    private Interactable interactable;
+
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    // Update is called once per frame
+    private void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint();
+    }
+    private void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint();
+    }
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes grabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        //GRAB THE OBJECT
+        if(interactable.attachedToHand == null && grabType != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabType);
+            hand.HoverLock(interactable);
+            hand.HideGrabHint();
+        }
+        else if(isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(interactable);
+        }
+    }
+
+}
